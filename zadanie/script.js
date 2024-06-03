2fb519d6a99362282cb64dab9634ad07ce807839
inFerno320 <infernolaki@gmail.com>
2024-06-03 20:52:57 +0200

    Add files via upload

---
+++ zadanie/script.js
@@ -0,0 +1,12 @@
+function open1() {
+    window.location.href = "a1.html";
+}
+function open2() {
+    window.location.href = "a2.html";
+}
+function open3() {
+    window.location.href = "a3.html";
+}
+function open4() {
+    window.location.href = "a4.html";
+}
