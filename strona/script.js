044d7569d4bc5ff9a629b2189a23e98a5caf57e5
inFerno320 <infernolaki@gmail.com>
2024-11-07 08:24:37 +0100

    Add files via upload

---
+++ strona/script.js
@@ -0,0 +1,12 @@
+function open1() {
+    window.location.href = "b1.html";
+}
+function open2() {
+    window.location.href = "b2.html";
+}
+function open3() {
+    window.location.href = "b3.html";
+}
+function open4() {
+    window.location.href = "b4.html";
+}
